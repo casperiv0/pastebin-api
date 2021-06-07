d15f3d43250604b5ba2bfeac3069e61d3e033abc
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-06-07 16:51:04 +0200

    :tada: tests: setup more tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,48 +1,88 @@
-import dotenv from "dotenv";
-import PasteClient from "../dist/index";
+import "dotenv/config";
+import test from "ava";
+import PasteClient, { CreateOptions } from "../dist";
 
-dotenv.config();
+// todo: add workflow for tests
 
-const KEY = process.env["TEST_API_KEY"];
-const USER_NAME = process.env["TEST_USER_NAME"];
-const USER_PASSWORD = process.env["TEST_USER_PASSWORD"];
+const TEST_DEV_KEY = process.env["TEST_DEV_KEY"] as string;
+const TEST_USER_NAME = process.env["TEST_USER_NAME"] as string;
+const TEST_USER_PASSWORD = process.env["TEST_USER_PASSWORD"] as string;
 
-if (!KEY) {
-  throw Error("No API key was provided for the test!");
-}
+let LOGIN_TOKEN: string | null = null;
 
-if (!USER_NAME || !USER_PASSWORD) {
-  throw Error("No user credentials were provided for the test");
-}
+const TEST_PASTE_DATA: CreateOptions = {
+  code: `const str = 'Hello World!' + ${Math.random() * 1000};`,
+  name: "Test Paste",
+  format: "javascript",
+  expireDate: "10M",
+};
 
-const client = new PasteClient(KEY);
+const client = new PasteClient(TEST_DEV_KEY);
 
-async function test() {
-  const token = await client.login(USER_NAME!, USER_PASSWORD!);
+test("Test should pass with an error '`apiKey` must be a string'", (t) => {
+  try {
+    new PasteClient("");
+    t.fail("No API_KEY was provided but the Client instantiated successfully");
+  } catch (e) {
+    if (e.message === "`apiKey` must be a string!") {
+      t.pass();
+    }
+  }
+});
 
-  // const deleted = await client.deletePasteByKey({
-  //   pasteKey: "XawZVJdA",
-  //   userKey: token,
-  // });
+test("It should create a new paste with a non-authenticated user", async (t) => {
+  try {
+    const pasteUrl = await client.createPaste(TEST_PASTE_DATA);
 
-  // console.log(deleted);
+    if (pasteUrl.startsWith("https")) {
+      return t.pass(`Paste URL: ${pasteUrl}`);
+    }
 
-  const pastes = await client.getPastesByUser({
-    userKey: token,
+    return t.fail(`An error has occurred: ${pasteUrl}`);
+  } catch (e) {
+    t.fail(e.message);
+  }
+});
+
+test("It should login with user credentials", async (t) => {
+  try {
+    const token = await client.login(TEST_USER_NAME, TEST_USER_PASSWORD);
+    LOGIN_TOKEN = token;
+
+    if (token) {
+      return t.pass();
+    }
+
+    return t.fail("An error has occurred");
+  } catch (e) {
+    t.fail(e.message);
+  }
+});
+
+test("It should get up too 1000 pastes of the authenticated user", async (t) => {
+  console.log("Waiting to make sure that we have a LOGIN_TOKEN..");
+
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
   });
 
-  console.log(pastes);
+  if (!LOGIN_TOKEN) {
+    return t.fail("LOGIN_TOKEN was null");
+  }
 
-  // const url = await client.createPaste({
-  //   code: "const x = 'hello world!'",
-  //   expireDate: "N",
-  //   format: "typescript",
-  //   name: "hello.ts",
-  //   publicity: 0,
-  //   apiUserKey: token,
-  // });
+  try {
+    const pastes = await client.getPastesByUser({ userKey: LOGIN_TOKEN });
 
-  // console.log(url);
-}
+    if (!pastes) {
+      return t.fail("Somehow there were no pastes");
+    }
 
-test();
+    if (pastes?.length <= 1000) {
+      return t.pass();
+    }
+
+    t.fail("Somehow there were more than 1000 pastes.");
+  } catch (e) {
+    t.fail(e.message);
+  }
+});
